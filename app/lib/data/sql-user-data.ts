c941999d59ba9378c036c465697dbdf8a07a4919
mlsloynaz <>
2024-04-15 11:41:53 -0400

    delete unused sql-query  functions

--- app/lib/data/sql-user-data.ts
+++ app/lib/data/sql-user-data.ts
@@ -2,8 +2,6 @@ import { sql } from '@vercel/postgres';
 import { User } from '../definitions';
 import { unstable_noStore as noStore } from 'next/cache';
 
-import bcrypt from "bcrypt";
-import { fromString } from 'uuidv4';
 
 //TODO: #10 Test fetching from database with mysql and  mssql, install packages and create clients. 
 
@@ -28,55 +26,6 @@ export async function fetchUserSql(id: string){
      try{
          const queryResponse = await sql<User>`SELECT * FROM users WHERE users.Id=${id}`;
          return queryResponse.rows[0];
-     }
-     catch(error){
-         console.error('Database Error:', error);
-         throw new Error('Failed to fetch users data.');
-     }
-} 
-
-export async function postUserSql(user: User){
-    try {
-        const hashedPassword = await bcrypt.hash(user.password, 10);
-        const idGenerated = await fromString(user.email + user.name);
-        const insertQuery = `INSERT INTO users (id, name, email, password)
-            VALUES (${idGenerated}, ${user.name}, ${user.email}, ${hashedPassword})
-            ON CONFLICT (id) DO NOTHING;
-        `;
-        // Log the SQL query
-        console.log('SQL Query:', insertQuery);
-        const queryResponse = await sql`${insertQuery}`;
-        return queryResponse.rowCount;
-    } catch (error) {
-        console.error('Database Error:', error);
-        throw new Error('Failed to create a user.');
-    }
-}
-
-
-export async function updateUserSql(user: User){
-    // the only ones that are cached by default are the GET , and when are not using the request object , ot cookies and header APIs
-    try{
-
-       const updateQuery =   `UPDATE users
-       SET name = ${user.name}, email = ${user.email}
-       WHERE ${user.id}= users.Id;
-     `
-       const queryResponse = await sql<User>`${updateQuery}`;
-       return queryResponse.rows;
-       
-    }
-    catch(error){
-        console.error('Database Error:', error);
-        throw new Error('Failed to update a user.');
-    }
-} 
-
-export async function deleteUserSql(id: string){
-   
-     try{
-         const queryResponse = await sql<User>`DELETE FROM users WHERE users.Id=${id}`;
-         return queryResponse.rows;
      }
      catch(error){
          console.error('Database Error:', error);
